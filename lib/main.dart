833bb60ead22543cc8c8aa1a3d11f0b4e55f251d
sunkyong23 <sunkyong23@gmail.com>
2026-05-31 17:45:51 +0900

    Firebase setup complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:image_cropper/image_cropper.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 class Post {
   final String imagePath;
@@ -30,7 +32,11 @@ class Post {
   });
 }
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const GroomingDayApp());
 }
 
